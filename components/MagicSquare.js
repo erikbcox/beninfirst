8e9206cc94d1839c2c1a0acf8d4cdfccee44daae
Erik Cox <erik.cox@gmail.com>
2018-11-14 13:08:25 -0600

    kill style

--- components/MagicSquare.js
+++ components/MagicSquare.js
@@ -13,23 +13,8 @@ export class MagicSquare extends React.Component {
   }
   render() {
 
-    style = {
-      domino :{
-        width: (this.state.width / 2)
-      },
-      equals: {
-        width: (this.state.width / 4)
-      },
-      answer:{        
-        width: (this.state.width / 4)
-      },
-      row:{
-        height: this.state.width / 4
-      }
-    }
-
     return <View style={{ flex: 1, flexDirection: 'column' }}>
-      <Text style={{fontSize:50, height:style.row.height*.90, textAlign:'center', justifyContent: 'center',}}>Magic Dominos</Text>
+      <Text style={{fontSize:50, height:(this.state.width / 4)*.90, textAlign:'center', justifyContent: 'center',}}>Magic Dominos</Text>
       <DominoRow leftValue={1} rightValue={4} width={this.state.width} />
       <DominoRow leftValue={6} rightValue={3} width={this.state.width} />
       <AnswerRow width={this.state.width} />
